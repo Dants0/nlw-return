62ed201dcabfe7348e0e0c70e414f1b0775dbe10
Dants0 <gui.dg1@hotmail.com>
2022-05-13 00:14:41 -0300

    resolvendo listen

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,30 +8,6 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
     console.log("Server running")
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// get buscar
-
-// post cadastrar infos
-
-// put atualizar infos
-
-// patch atualizar info unica de uma entidade
-
-// delete deletar uma info
